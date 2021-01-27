320e98f85cc41b249f6aff5deeb66ddd975a2a58
RachelUberti <rachel.uberti@gmail.com>
2021-01-27 12:36:15 +1100

    set variables

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// alert("working!");
+
+
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+// Random number between 1 and 6, rounded up to nearest number. To check in console type "Math.random()"
+
+let randomDiceImage = "image/dice" + randomNumber1 + ".png"
